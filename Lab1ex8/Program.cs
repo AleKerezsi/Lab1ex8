189b7b0032dad104c1816c165978ac809bf34d2d
alexandra.kerezsi.18@gmail.com <alexandra.kerezsi.18@gmail.com>
2022-11-27 21:41:04 +0200

    Add project files.

---
+++ Lab1ex8/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+
+namespace Lab1ex8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Se citesc doua numere de la tastatura, x si y. Scrieti un program care va verifica daca x este divizibil cu y. */
+
+            Console.WriteLine("Introduceti x");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduceti y");
+            int y = int.Parse(Console.ReadLine());
+
+          
+            if (x % y == 0)  Console.WriteLine("Divizibil");
+            else Console.WriteLine("Indivizibil");
+
+        }
+    }
+}
